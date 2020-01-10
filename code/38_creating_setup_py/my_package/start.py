6216f99a33b374ccd57da1eae029481ae8778822
Aquiles Carattino <aquiles@uetke.com>
2020-01-10 16:02:31 +0100

    Added console entry point

---
+++ code/38_creating_setup_py/my_package/start.py
@@ -0,0 +1,7 @@
+from .mod_c.file_c import function_c
+
+
+def main():
+    function_c()
+    print('This is main')
+
--- code/38_creating_setup_py/setup.py
+++ code/38_creating_setup_py/setup.py
@@ -3,5 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='My First Setup File',
     version='1.1',
-    packages=find_packages()
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'my_start=my_package.start:main',
+        ]
+    }
 )
