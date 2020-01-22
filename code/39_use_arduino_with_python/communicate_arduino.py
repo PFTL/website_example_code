e7c9c1e225a9c575bb1a34f2a895939fde69ec10
Aquiles Carattino <aqui.carattino@gmail.com>
2020-01-22 11:55:00 +0100

    Made Arduino Class

--- code/39_use_arduino_with_python/communicate_arduino.py
+++ code/39_use_arduino_with_python/communicate_arduino.py
@@ -2,14 +2,27 @@ import serial
 from time import sleep
 
 
-dev = serial.Serial("COM4", baudrate=19200)
-sleep(1)
+class Arduino:
+    def __init__(self, port):
+        self.dev = serial.Serial(port, baudrate=19200)
+        sleep(1)
+
+    def query(self, message):
+        self.dev.write(message.encode('ascii'))
+        line = self.dev.readline().decode('ascii').strip()
+        return line
+
+
+ard = Arduino('COM4')
+
+for _ in range(10):
+    print(ard.query('1'))
+    sleep(0.5)
+    print(ard.query('0'))
+    sleep(0.5)
 
 data = []
 for _ in range(10):
-    dev.write(b'2')
-    line = dev.readline()
-    line = int(line.decode('ascii'))
-    data.append(line)
+    data.append(ard.query('2'))
 
 print(data)
