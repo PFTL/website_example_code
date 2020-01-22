86b207ab9f295a2230bd2dc05ad691953f5180f9
Aquiles Carattino <aqui.carattino@gmail.com>
2020-01-22 11:36:47 +0100

    Simple loop to acquire data

--- code/39_use_arduino_with_python/communicate_arduino.py
+++ code/39_use_arduino_with_python/communicate_arduino.py
@@ -4,10 +4,11 @@ from time import sleep
 
 dev = serial.Serial("COM4", baudrate=19200)
 sleep(1)
-dev.write(b'1')
-print(dev.readline())
-dev.write(b'0')
-print(dev.readline())
-dev.write(b'1')
-print(dev.readline())
 
+data = []
+for _ in range(10):
+    dev.write(b'2')
+    line = dev.readline()
+    data.append(line)
+
+print(data)
