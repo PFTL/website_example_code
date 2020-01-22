a5be5ddde50d5ecf74f80c4e24b12dbef6397cdf
Aquiles Carattino <aqui.carattino@gmail.com>
2020-01-22 11:32:42 +0100

    Script with sleep after opening

--- code/39_use_arduino_with_python/communicate_arduino.py
+++ code/39_use_arduino_with_python/communicate_arduino.py
@@ -1,10 +1,13 @@
 import serial
+from time import sleep
 
 
 dev = serial.Serial("COM4", baudrate=19200)
+sleep(1)
 dev.write(b'1')
 print(dev.readline())
 dev.write(b'0')
 print(dev.readline())
 dev.write(b'1')
 print(dev.readline())
+
