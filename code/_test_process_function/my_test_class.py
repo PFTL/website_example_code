b5ddde470d2638da1121d85a8ba7f5dfbec617ed
Aquiles Carattino <aqui.carattino@gmail.com>
2020-03-11 17:13:53 +0100

    Context manager for process class

--- code/_test_process_function/my_test_class.py
+++ code/_test_process_function/my_test_class.py
@@ -27,14 +27,12 @@ class MyTestClass:
         for i in range(self.param1):
             print(i)
 
-    def __del__(self):
-        print('Deleting the class')
-
 
 class ProcessClass:
     def __init__(self, cls, stop_event, *args, **kwargs):
         self.cls = cls
         self.method_queue = Queue()
+        self.stop_event = stop_event
         if kwargs:
             kwargs.update({'stop_event': stop_event, 'method_queue': self.method_queue})
         else:
@@ -54,14 +52,22 @@ class ProcessClass:
                 return empty_func
             raise
 
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print('Exiting the Process Class')
+        self.stop_event.set()
+        self.process.join(timeout=5)
+
 
 if __name__ == '__main__':
     stop_event = Event()
     stop_event.clear()
     method_queue = Queue()
-    pc = ProcessClass(MyTestClass, stop_event, 50, 20)
-    pc.acquire()
-    for i in range(10):
-        if pc.process.is_alive():
-            time.sleep(.1)
-    stop_event.set()
+
+    with ProcessClass(MyTestClass, stop_event, 50, 20) as pc:
+        pc.acquire()
+        for i in range(10):
+            if pc.process.is_alive():
+                time.sleep(.1)
