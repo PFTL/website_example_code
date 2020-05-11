5c27147fd37828775e44b2041b6f70d8e8634fe6
Aquiles Carattino <aqui.carattino@gmail.com>
2020-05-11 22:28:52 +0200

    Updated code

--- code/42_descriptors/ac_set_name/ab.py
+++ code/42_descriptors/ac_set_name/ab.py
@@ -11,29 +11,20 @@ class MyDescriptor:
 
         owner._descriptors.append(name)
 
-    def __get__(self, instance, owner):
-        print('Getting Descriptor')
-        return self.fget(instance)
-
-    def __set__(self, instance, value):
-        print('Setting Descriptor')
-        self.fset(instance, value)
-
-    def setter(self, fset):
-        return type(self)(self.fget, fset)
-
 
 class MyClass:
     _var = 0
+    _var1 = 0
 
     @MyDescriptor
     def var(self):
         return self._var
 
-    @var.setter
-    def var(self, value):
-        self._var = value
+    @MyDescriptor
+    def var1(self):
+        return self._var1
 
 
 my_class = MyClass()
 print(my_class._descriptors)
+print(my_class.var.fget(my_class))
